a24a0106458907b5f50c78f31c96e481094bc1bb
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-18 21:29:25 +0000

    Add checks for gender and age

--- routers/users.js
+++ routers/users.js
@@ -5,47 +5,42 @@ const sequelize = db.sequelize;
 const User = db.User;
 const Profile = db.Profile;
 
-attr3: {
-  in: [1,2,3]
 
 router.get("/", (req, res, next) => {
-  const formParams = req.query.form;
-  User.findAll({
-    include: [
-      {
-        model: Profile,
-        where: {
-          gender: formParams.gender,
-          marital: {
-            in: formParams.marital
-          }
-        }
-      }
-    ]
-  })
-    .then(users => {
-      res.render("users/index", {
-        users
-      });
-    })
-    .catch(err => next(err));
+    const formParams = req.query.form ? req.query.form : {};
+
+    User.findAll({
+            include: [{
+                model: Profile,
+                where: {
+                    gender: formParams.gender || { in: ['male', 'female', 'pterodactyl']
+                    },
+                    marital: { in: formParams.marital || ['single', 'dating', 'married']
+                    }
+                }
+            }]
+        })
+        .then(users => {
+            res.render("users/index", {
+                users
+            });
+        })
+        .catch(err => next(err));
 });
 
 router.get("/:id", (req, res, next) => {
-  let userId = req.params.id;
-  User.findById(userId, {
-    include: [
-      {
-        model: Profile
-      }
-    ]
-  })
-    .then(user => {
-      res.render("users/show", {
-        user
-      });
-    })
-    .catch(err => next(err));
+    let userId = req.params.id;
+    User.findById(userId, {
+            include: [{
+                model: Profile
+            }]
+        })
+        .then(user => {
+            res.render("users/show", {
+                user
+            });
+        })
+        .catch(err => next(err));
 });
 
 module.exports = router;
