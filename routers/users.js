0a88993cad259127805c8e812abb91a5c008392d
Jerry Gao <gao@blackwright.com>
2017-04-18 17:19:48 -0400

    start form stuff

--- routers/users.js
+++ routers/users.js
@@ -5,35 +5,47 @@ const sequelize = db.sequelize;
 const User = db.User;
 const Profile = db.Profile;
 
+attr3: {
+  in: [1,2,3]
+
 router.get("/", (req, res, next) => {
-    User.findAll({
-            include: [{
-                model: Profile
-            }]
-        })
-        .then(users => {
-            res.render("users/index", {
-                users
-            });
-        })
-        .catch(err => next(err));
+  const formParams = req.query.form;
+  User.findAll({
+    include: [
+      {
+        model: Profile,
+        where: {
+          gender: formParams.gender,
+          marital: {
+            in: formParams.marital
+          }
+        }
+      }
+    ]
+  })
+    .then(users => {
+      res.render("users/index", {
+        users
+      });
+    })
+    .catch(err => next(err));
 });
 
 router.get("/:id", (req, res, next) => {
-    let userId = req.params.id;
-    User.findById(userId, {
-            include: [{
-                model: Profile
-            }]
-        })
-        .then(user => {
-            res.render("users/show", {
-                user
-            });
-        })
-        .catch(err => next(err));
+  let userId = req.params.id;
+  User.findById(userId, {
+    include: [
+      {
+        model: Profile
+      }
+    ]
+  })
+    .then(user => {
+      res.render("users/show", {
+        user
+      });
+    })
+    .catch(err => next(err));
 });
-
-
 
 module.exports = router;
